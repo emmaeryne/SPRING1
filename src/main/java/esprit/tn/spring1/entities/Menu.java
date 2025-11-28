752f4b9d6bb4758d4ba208850e2e6d66fd72ab4b
emma awini <94049201+emmaeryne@users.noreply.github.com>
2025-11-28 10:40:12 +0100

    POSTMMAN

--- src/main/java/esprit/tn/spring1/entities/Menu.java
+++ src/main/java/esprit/tn/spring1/entities/Menu.java
@@ -153,7 +153,7 @@ public class Menu {
                 '}';
     }
 }*/
-package esprit.tn.spring1.entities;
+/*package esprit.tn.spring1.entities;
 
 import esprit.tn.spring1.enums.TypeMenu;
 import jakarta.persistence.*;
@@ -184,6 +184,59 @@ public class Menu {
 
     @ManyToOne
     @JoinColumn(name = "idRestaurant")
+    private Restaurant restaurant;
+
+    @OneToMany(mappedBy = "menu", cascade = CascadeType.ALL)
+    private List<Composant> composants;
+
+    @Override
+    public String toString() {
+        return "Menu{" +
+                "idMenu=" + idMenu +
+                ", libelleMenu='" + libelleMenu + '\'' +
+                ", typeMenu=" + typeMenu +
+                ", prixTotal=" + prixTotal +
+                '}';
+    }
+}*/
+
+package esprit.tn.spring1.entities;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import esprit.tn.spring1.enums.TypeMenu;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Menu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonProperty("id_menu")
+    private Long idMenu;
+
+    @JsonProperty("libelle_menu")
+    private String libelleMenu;
+
+    @Enumerated(EnumType.STRING)
+    @JsonProperty("type_menu")
+    private TypeMenu typeMenu;
+
+    @JsonProperty("prix_total")
+    private Float prixTotal;
+
+    @ManyToOne
+    @JoinColumn(name = "idRestaurant")
+    @JsonProperty("id_restaurant")
     private Restaurant restaurant;
 
     @OneToMany(mappedBy = "menu", cascade = CascadeType.ALL)
